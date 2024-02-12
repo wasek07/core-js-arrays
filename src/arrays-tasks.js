6ed2c09179affc411568c957615f9cfb0d292f4d
wasek07 <wasek07@ya.ru>
2024-02-12 22:48:42 +0100

    feat: add solution for falsy values count

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -378,8 +378,9 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  const falsyValuesCount = arr.length - arr.filter(Boolean).length;
+  return falsyValuesCount;
 }
 
 /**
